6e3408ee00d038cef003d8648f8eebfdde0d8b33
Shavkat Urozboev <72398904+shavkat1315@users.noreply.github.com>
2022-04-05 10:28:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+01-dars. Hello world by Anvar Narzullayev
+
+Created on Tue Apr  5 14:06:26 2022
+
+@author: Urozboev Shavkat
+
+"""
+
+print("Hello World!")
+print(7+8)
+#                Practice 
+print('Assalom alaykum')
+#print(Hayrli tong!)
+print(2+4*2)
+print(19/5)
+print(2**4)
+
+#               Amaliy mashg'ulot
+
+# 1- Misol
+print('Qulay usul bilan hisoblang:')
+print("""1) (38*54+38*42)/24; 3) 736*983-736*883; 
+2) 2_416*67+33*2_416; 4) (88*89-88*69)/440+60.""")
+print("Start:")
+# 1
+print( "1) (38*54+38*42)/24;")
+print("Solution:")
+print("First step: a*b+a*c=a*(b*c)")
+d=38*(54+42)
+print("38*54+38*42=38*(54+42)=",d)
+print(d,"/ 24 =",d/24)
+print("Answer:",d/24)
+# 2
+print( "2) 2_416*67+33*2_416;")
+print("Solution:")
+print("First step: a*b+a*c=a*(b*c)")
+d=2_416*(67+33)
+print("2_416*67+33*2_416=2_416*(67+33)=",d)
+print("Answer:",d)
+# 3
+print( "3) 736*983-736*883;")
+print("Solution:")
+print("First step: a*b+a*c=a*(b*c)")
+d=736*(983-883)
+print("736*983-736*883=736*(983-883)=",d)
+print("Answer:",d)
+# 4
+print( "4) (88*89-88*69)/440+60")
+print("Solution:")
+print("First step: a*b+a*c=a*(b*c)")
+d=88*(89-69)
+print("88*89-88*69=88*(89-69)=",d)
+e=d/440
+print(d,"/ 440 =",e)
+f=e+60
+print(e,"+60=",f)
+print("Answer:",f)
